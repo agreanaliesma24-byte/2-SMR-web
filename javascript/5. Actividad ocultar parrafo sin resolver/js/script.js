f08eed5b8c813aee83f6f6390a33486a16a39050
agreanaliesma24-byte <agrean_aliesma24@iesmedinaazahara.es>
2026-05-26 13:24:52 +0200

    a

---
+++ javascript/5. Actividad ocultar parrafo sin resolver/js/script.js
@@ -0,0 +1,15 @@
+// Selección de los elementos del DOM
+const boton = document.getElementById('   ');
+const textoExtra = document.getElementById('   ');
+
+// Escuchador de eventos para el click
+boton.addEventListener('click', () => {
+    // Comprobamos el estado actual del párrafo
+    if (textoExtra.style.display === 'block') {
+        // Si es visible, lo ocultamos y cambiamos el texto
+        
+    } else {
+        // Si está oculto, lo mostramos y cambiamos el texto
+       
+    }
+});
